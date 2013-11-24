1496a234e05359dc8c1c899295170bd4adb15dc8
Eric Stiens <estiens@server.fake>
2013-11-23 21:36:23 -0600

    refactoring board tests to not use magic numbers

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,4 +1,7 @@
 require_relative 'spec_helper'
+  X = 1
+  O = -1
+  N = 0
 
 describe Board do
 
@@ -30,8 +33,8 @@ describe Board do
   end
 
   it "marks a square and retrieves that value" do
-    board.mark_square(1,1,1)
-    expect(board.find_mark_of_square(1,1)).to eq(1)
+    board.mark_square(1,1,X)
+    expect(board.find_mark_of_square(1,1)).to eq(X)
   end
 
   it "checks for a tie and returns false when no tie" do
@@ -39,7 +42,7 @@ describe Board do
   end
 
   it "checks for a tie and returns true when there is a tie" do
-    board.board=[[1,-1,1],[-1,1,1],[-1,1,-1]]
+    board.board=[[X,O,X],[O,X,O],[O,X,O]]
     expect(board.tie?).to eq true
   end
 
@@ -52,17 +55,17 @@ describe Board do
   describe "it checks for a horizontal winner" do
 
     it "returns true if the top row is filled" do
-      board.board=[[1,1,1],[0,0,0],[0,0,0]]
+      board.board=[[X,X,X],[N,N,N],[N,N,N]]
       expect(board.winner?).to eq(true)
     end
 
     it "returns true if the middle row is filled" do
-      board.board=[[0,0,0], [-1,-1,-1], [0,0,0]]
+      board.board=[[N,N,N], [O,O,O], [N,N,N]]
       expect(board.winner?).to eq(true)
     end
 
     it "returns true if the bottom row is filled" do
-      board.board=[[0,0,0], [0,0,0], [1,1,1]]
+      board.board=[[N,N,N], [N,N,N], [X,X,X]]
       expect(board.winner?).to eq(true)
     end
 
@@ -71,17 +74,17 @@ describe Board do
   describe "it checks for a vertical winner" do
 
     it "returns true if left column is filled" do
-      board.board=[[-1,0,0], [-1,0,0], [-1,0,0]]
+      board.board=[[O,N,N], [O,N,N], [O,N,N]]
       expect(board.winner?).to eq(true)
     end
 
     it "returns true if the middle column is filled" do
-      board.board=[[0,1,0], [0,1,0], [0,1,0]]
+      board.board=[[N,X,N], [N,X,N], [N,X,N]]
       expect(board.winner?).to eq(true)
     end
 
     it "returns true if the right column is filled" do
-      board.board=[[0,0,-1], [0,0,-1], [0,0,-1]]
+      board.board=[[N,N,O], [N,N,O], [N,N,O]]
       expect(board.winner?).to eq(true)
     end
 
@@ -90,12 +93,12 @@ describe Board do
   describe "it checks for a diagonal winner" do
 
     it "returns true if the upper left-lower right diagonal is filled" do
-      board.board=[[1,0,0],[0,1,0],[0,0,1]]
+      board.board=[[X,N,N],[N,X,N],[N,N,X]]
       expect(board.winner?).to eq true
     end
 
     it "returns true if the upper right-lower left diagonal is filled" do
-      board.board=[[0,0,-1],[0,-1,0],[-1,0,0]]
+      board.board=[[N,N,O],[N,O,N],[O,N,N]]
       expect(board.winner?).to eq true
     end
 
@@ -104,17 +107,17 @@ describe Board do
   describe "it passes cursory checks for a 4x4 board" do
 
     it "returns true if there is a horizontal winner" do
-      board4.board=[1,0,0,0],[1,0,0,0],[1,0,0,0],[1,0,0,0]
+      board4.board=[X,N,N,N],[X,N,N,N],[X,N,N,N],[X,N,N,N]
       expect(board4.winner?).to eq true
     end
 
     it "returns true if there is a vertical winner" do
-      board4.board=[[-1,-1,-1,-1],[0,0,0,0],[0,0,0,0],[0,0,0,0]]
+      board4.board=[[O,O,O,O],[N,N,N,N],[N,N,N,N],[N,N,N,N]]
       expect(board4.winner?).to eq true
     end
 
     it "returns true if there is a diagonal winner" do
-      board4.board=[[-1,0,0,0],[0,-1,0,0],[0,0,-1,0],[0,0,0,-1]]
+      board4.board=[[O,N,N,N],[N,O,N,N],[N,N,O,N],[N,N,N,O]]
       expect(board4.winner?).to eq true
     end
 
@@ -126,7 +129,7 @@ describe Board do
     end
 
     it "returns all valid moves when some are taken" do
-      board.board=[[1,1,1], [-1,-1,-1], [0,0,0]]
+      board.board=[[X,X,X], [O,O,O], [N,N,N]]
       expect(board.all_valid_moves).to eq([[2,0],[2,1],[2,2]])
     end
   end
@@ -136,15 +139,23 @@ describe Board do
       expect(board.empty?(0,0)).to eq true
     end
     it "returns false when a space is not empty" do
-      board.mark_square(0,0,1)
+      board.mark_square(0,0,X)
       expect(board.empty?(0,0)).to eq false
+    end
+  end
+
+  describe '#undo' do
+    it "resets the selected space to NONE" do
+    board.mark_square(1,1,X)
+    board.undo(1,1)
+    expect(board.find_mark_of_square(1,1)).to eq 0
     end
   end
 
   describe '#clear_squares' do
     it "clears all the squares on the board" do
       board.mark_square(1,1,1)
-      board.mark_square(1,2,-1)
+      board.mark_square(1,2,O)
       board.clear_squares
       expect(board.board.flatten.uniq).to eq([0])
     end
